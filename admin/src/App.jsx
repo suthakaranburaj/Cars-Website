d1a7112ea29c6975254035e8035955a8d4821625
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-16 11:59:10 +0530

    admin panel added successfully !!

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,7 @@ import Admin from "./Pages/Admin/Admin";
 
 export const backend_url = 'http://localhost:4000';
 export const currency = '₹';
-
+//
 function App() {
   return (
     <BrowserRouter>
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import AddProduct from "../../Components/AddProduct/AddProduct";
 import { Route, Routes } from "react-router-dom";
 import ListProduct from "../../Components/ListProduct/ListProduct";
-
+//
 const Admin = () => {
   return (
     <div className="admin">
